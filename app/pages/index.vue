0d4c46011be804357762e421bfc9ef87a94fd2bd
Edouard <edurand.viel@gmail.com>
2025-12-09 17:26:59 +0100

    separator

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -57,6 +57,8 @@
     </div>
   </div>
 
+  <div class="separator"></div>
+
   <div class="bg-black py-vh-3">
     <div class="container bg-black px-vw-5 py-vh-3 rounded-5 shadow">
       <div class="row gx-5">
@@ -153,144 +155,8 @@
       </div>
     </div>
   </div>
-  <div class="bg-dark position-relative">
-    <div class="container px-vw-5 py-vh-5">
-      <div class="row d-flex align-items-center">
-        <div class="col-12 col-lg-7">
-          <img
-            class="img-fluid rounded-5 mb-n5 shadow"
-            src="/img/webp/person9.webp"
-            width="512"
-            height="512"
-            alt="a nice person"
-            loading="lazy"
-            data-aos="zoom-in-right"
-          />
-          <img
-            class="img-fluid rounded-5 ms-5 mb-n5 shadow"
-            src="/img/webp/person11.webp"
-            width="512"
-            height="512"
-            alt="another nice person"
-            loading="lazy"
-            data-aos="zoom-in-up"
-          />
-        </div>
-        <div class="col-12 col-lg-5 bg-dark rounded-5 py-5" data-aos="fade">
-          <span class="h5 text-secondary fw-lighter">Do you like faces?</span>
-          <h2 class="display-4">
-            We constantly adding new images to our website. Does it make sense? No!
-          </h2>
-        </div>
-      </div>
-    </div>
-  </div>
+ 
 
-  <div class="bg-black">
-    <div class="container px-vw-5 py-vh-5">
-      <div class="row d-flex align-items-center">
-        <div class="col-12 col-lg-5 text-center text-lg-end" data-aos="zoom-in-right">
-          <span class="h5 text-secondary fw-lighter">What we charge</span>
-          <h2 class="display-4">You get all our knowledge for one simple price</h2>
-        </div>
-        <div
-          class="col-12 col-lg-7 bg-dark rounded-5 py-vh-3 text-center my-5"
-          data-aos="zoom-in-up"
-        >
-          <h2 class="display-huge mb-5">
-            <span class="fs-4 me-2 fw-light">$</span><span class="border-bottom border-5">499</span
-            ><span class="fs-6 fw-light">/day</span>
-          </h2>
-          <p class="lead text-secondary">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          </p>
-          <a href="#" class="btn btn-xl btn-light"
-            >Sign up
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-arrow-right"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-              />
-            </svg>
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="container-fluid px-vw-5 position-relative" data-aos="fade">
-    <div class="position-absolute w-100 h-50 bg-black top-0 start-0"></div>
-    <div
-      class="position-relative py-vh-5 bg-cover bg-center rounded-5"
-      style="background-image: url(/img/webp/abstract12.webp)"
-    >
-      <div class="container bg-black px-vw-5 py-vh-3 rounded-5 shadow">
-        <div class="row d-flex align-items-center">
-          <div
-            class="col-6 d-flex align-items-center bg-dark shadow rounded-5 p-0"
-            data-aos="zoom-in-up"
-          >
-            <div class="row d-flex justify-content-center">
-              <div class="col-12">
-                <img
-                  src="/img/webp/person103.webp"
-                  width="684"
-                  height="457"
-                  alt="our CEO with some nice numbers"
-                  class="img-fluid rounded-5"
-                  loading="lazy"
-                />
-              </div>
-              <div class="col-12 col-lg-10 col-xl-8 text-center my-5">
-                <p class="lead border-bottom pb-4 border-secondary">
-                  "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                  tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-                  vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-                  no sea takimata sanctus est Lorem ipsum dolor sit amet."
-                </p>
-                <p class="text-secondary text-center">Jane Doe, CEO</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-5 offset-1">
-            <span class="h5 text-secondary fw-lighter">The numbers</span>
-            <h2 class="display-huge fw-bolder" data-aos="zoom-in-left">+400</h2>
-            <p class="h4 fw-lighter text-secondary">
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
-            </p>
-
-            <h2
-              class="display-huge fw-bolder border-top border-secondary pt-5 mt-5"
-              data-aos="zoom-in-left"
-            >
-              78.981
-            </h2>
-            <p class="h4 fw-lighter text-secondary">
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
-            </p>
-            <h2
-              class="display-huge fw-bolder border-top border-secondary pt-5 mt-5"
-              data-aos="zoom-in-left"
-            >
-              98%
-            </h2>
-            <p class="h4 fw-lighter text-secondary">
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script setup lang="ts">
